19f063fea143f607bf1ff9316b3a2cd425be3f0b
Atul <atul.shrwstv123@gmail.com>
2022-02-23 22:33:35 +0545

    third commit from another branch

--- calc.c
+++ calc.c
@@ -3,3 +3,5 @@ Program Name: calculator.c
 Program Description: this is a calculator project.
 Program Author: Atul Shreewastav
 ********************************************************************************************************/
+
+#include <stdio.h> //from experiment branch
